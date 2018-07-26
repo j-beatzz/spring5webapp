21d8c6d08e8eadaedbdbfd790aa3fa814f229052
Joshua LEO-Irabor <joshua.l@ecobee.com>
2018-07-26 15:00:44 -0400

    Refactored controller to make use of @Autowired

--- src/main/java/irab/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/irab/springframework/spring5webapp/controllers/BookController.java
@@ -1,17 +1,15 @@
 package irab.springframework.spring5webapp.controllers;
 
 import irab.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
+    @Autowired
     private BookRepository bookRepository;
-
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     @RequestMapping("/books")
     public String getBooks(Model model) {
